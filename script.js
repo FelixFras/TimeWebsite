216eb5144223f869ace4f0947e50cd3f8d5a6a9e
FelixFras <f.frasch@gmx.net>
2024-08-12 18:28:57 +0200

    added timezone

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+let timezone = parseInt(0);
+
 function updateClock() {
     const now = new Date();
-    const hours = now.getHours();
+    const hours = now.getHours() + timezone;
     const minutes = now.getMinutes().toString().padStart(2, '0');
     const seconds = now.getSeconds().toString().padStart(2, '0');
     const time = `${hours}:${minutes}:${seconds}`;
@@ -17,6 +19,11 @@ function makeBigger() {
     const clockElement = document.getElementById('clock');
     const currentSize = parseFloat(window.getComputedStyle(clockElement).fontSize);
     clockElement.style.fontSize = (currentSize + 10) + 'px';
+}
+
+function changeTimezone() {
+    timezone = parseInt(document.getElementById('timezone').value);
+    console.log(timezone);
 }
 
 setInterval(updateClock, 100); // Update every 100 ms for accuracy
