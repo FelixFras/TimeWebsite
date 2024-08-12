433d3636d09fda034819bfa265e46ad081b9c29f
FelixFras <f.frasch@gmx.net>
2024-08-12 18:01:11 +0200

    added '0' before minute and second single digits added style.css file to center div and set font

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function updateClock() {
     const now = new Date();
     const hours = now.getHours();
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
     const time = `${hours}:${minutes}:${seconds}`;
     document.getElementById('clock').innerText = time;
 }
 
-setInterval(updateClock, 1); // Update every ms
+setInterval(updateClock, 100); // Update every 100 ms for accuracy
